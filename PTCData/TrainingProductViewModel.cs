10e5edc2d431e3d80925d6c158010db6595e6ed6
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-07-04 12:40:28 +0200

    Some more progress and testing the Visual Studio Git method of pushing to Github

--- PTCData/TrainingProductViewModel.cs
+++ PTCData/TrainingProductViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace PTCData
 {
@@ -6,23 +7,63 @@ namespace PTCData
     {
         public TrainingProductViewModel()
         {
-            IsValid = true;
-            IsDetailAreaVisible = false;
-            IsListAreaVisible = true;
-            IsSearchAreaVisible = true;
+            Init();
             // Initialize blank list
             Products = new List<TrainingProduct>();
-            SearchEntity = new TrainingProduct();
-            EventCommand = "List";
+            SearchEntity = new TrainingProduct();            
         }
 
         public List<TrainingProduct> Products { get; set; }
         public TrainingProduct SearchEntity { get; set; }
+        public TrainingProduct Entity { get; set; }
+        public List<KeyValuePair<string, string>> ValidationErrors { get; set; }
+
+        public string Mode { get; set; }
         public string EventCommand { get; set; }
         public bool IsValid { get; set; }
         public bool IsDetailAreaVisible { get; set; }
         public bool IsListAreaVisible { get; set; }
         public bool IsSearchAreaVisible { get; set; }
+
+        private void Init()
+        {
+            EventCommand = "List";
+
+            ListMode();
+        }
+
+        private void ListMode()
+        {
+            IsValid = true;
+            IsDetailAreaVisible = false;
+            IsListAreaVisible = true;
+            IsSearchAreaVisible = true;
+
+            Mode = "List";
+        }
+
+        private void AddMode()
+        {
+            IsDetailAreaVisible = true;
+            IsListAreaVisible = false;
+            IsSearchAreaVisible = false;
+
+            Mode = "Add";
+        }
+
+        private void Add()
+        {
+            IsValid = true;
+
+            // Initialize Entity Object
+            Entity = new TrainingProduct();
+            Entity.IntroductionDate = DateTime.Now;
+            Entity.Url = string.Empty;
+            Entity.Price = 0;
+
+            // Put ViewModel into Add Mode
+            AddMode();
+        }
 
         public void HandleRequest()
         {
@@ -33,16 +74,51 @@ namespace PTCData
                     Get();
                     break;
 
+                case "save":
+                    Save();
+                    Get();
+                    break;
+
+                case "cancel":
+                    IsDetailAreaVisible = false;
+                    IsListAreaVisible = true;
+                    IsSearchAreaVisible = true;
+                    Get();
+                    break;
+
                 case "add":
-                    IsDetailAreaVisible = true;
-                    IsListAreaVisible = false;
-                    IsSearchAreaVisible = false;
+                    Add();
                     break;
 
                 case "resetsearch":
                     ResetSearch();
                     Get();
                     break;
+            }
+        }
+
+        private void Save()
+        {
+            TrainingProductManager mgr =
+        new TrainingProductManager();
+
+            if (Mode == "Add")
+            {
+                mgr.Insert(Entity);
+            }
+            // Set any validation errors
+            ValidationErrors = mgr.ValidationErrors;
+            if (ValidationErrors.Count > 0)
+            {
+                IsValid = false;
+            }
+
+            if (!IsValid)
+            {
+                if (Mode == "Add")
+                {
+                    AddMode();
+                }
             }
         }
 
