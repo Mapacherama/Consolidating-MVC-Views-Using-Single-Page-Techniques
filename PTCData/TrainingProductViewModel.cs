9c26721d98c6b29db4152476665161a70e3bf87f
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-07-04 10:54:45 +0200

    Some more progress.

--- PTCData/TrainingProductViewModel.cs
+++ PTCData/TrainingProductViewModel.cs
@@ -2,46 +2,60 @@
 
 namespace PTCData
 {
-  public class TrainingProductViewModel
-  {
-    public TrainingProductViewModel()
+    public class TrainingProductViewModel
     {
-      // Initialize blank list
-      Products = new List<TrainingProduct>();
-      SearchEntity = new TrainingProduct();
-      EventCommand = "List";
+        public TrainingProductViewModel()
+        {
+            IsValid = true;
+            IsDetailAreaVisible = false;
+            IsListAreaVisible = true;
+            IsSearchAreaVisible = true;
+            // Initialize blank list
+            Products = new List<TrainingProduct>();
+            SearchEntity = new TrainingProduct();
+            EventCommand = "List";
+        }
+
+        public List<TrainingProduct> Products { get; set; }
+        public TrainingProduct SearchEntity { get; set; }
+        public string EventCommand { get; set; }
+        public bool IsValid { get; set; }
+        public bool IsDetailAreaVisible { get; set; }
+        public bool IsListAreaVisible { get; set; }
+        public bool IsSearchAreaVisible { get; set; }
+
+        public void HandleRequest()
+        {
+            switch (EventCommand.ToLower())
+            {
+                case "list":
+                case "search":
+                    Get();
+                    break;
+
+                case "add":
+                    IsDetailAreaVisible = true;
+                    IsListAreaVisible = false;
+                    IsSearchAreaVisible = false;
+                    break;
+
+                case "resetsearch":
+                    ResetSearch();
+                    Get();
+                    break;
+            }
+        }
+
+        private void ResetSearch()
+        {
+            SearchEntity = new TrainingProduct();
+        }
+
+        private void Get()
+        {
+            TrainingProductManager mgr = new TrainingProductManager();
+
+            Products = mgr.Get(SearchEntity);
+        }
     }
-
-    public List<TrainingProduct> Products { get; set; }
-    public TrainingProduct SearchEntity { get; set; }
-    public string EventCommand { get; set; }
-
-    public void HandleRequest()
-    {
-      switch (EventCommand.ToLower())
-      {
-        case "list":
-        case "search":
-          Get();
-          break;
-
-        case "resetsearch":
-          ResetSearch();
-          Get();
-          break;
-      }
-    }
-
-    private void ResetSearch()
-    {
-      SearchEntity = new TrainingProduct();
-    }
-
-    private void Get()
-    {
-      TrainingProductManager mgr = new TrainingProductManager();
-
-      Products = mgr.Get(SearchEntity);
-    }
-  }
 }
